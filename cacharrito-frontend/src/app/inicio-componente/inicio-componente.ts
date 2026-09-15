d28934696ed65a705a363f5da95d894d06568f4f
leidyvillegas59609-lab <leidy.villegas59609@ucaldas.edu.co>
2026-09-14 23:43:02 -0500

    Creación Entidad y componente de Administrador  en el frontend

---
+++ cacharrito-frontend/src/app/inicio-componente/inicio-componente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-inicio-componente',
+  styleUrl: './inicio-componente.css',
+  templateUrl: './inicio-componente.html',
+})
+export class InicioComponente {}
