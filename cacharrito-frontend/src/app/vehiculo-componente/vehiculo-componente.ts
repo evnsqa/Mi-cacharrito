d0e7964d57eaa95373460b19cc6cd237127c66ca
011-ML <mariaidaliperez@gmail.com>
2026-09-15 03:08:49 -0500

    Creación Entidad y componente de Vehiculo en el frontend

---
+++ cacharrito-frontend/src/app/vehiculo-componente/vehiculo-componente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-vehiculo-componente',
+  styleUrl: './vehiculo-componente.css',
+  templateUrl: './vehiculo-componente.html',
+})
+export class VehiculoComponente {}
